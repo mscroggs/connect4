0f8af3a2bd623449579c9c45b0f1790800cf72b8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-06-15 17:21:29 +0100

    simplified play.py example

--- play.py
+++ play.py
@@ -4,15 +4,11 @@ from core import Game
 
 from strategies.random_strategy import RandomStrategy
 from strategies.lowest_column import LowestColumnStrategy
-from strategies.vertical_search import VerticalSearcher
 from strategies.human_play import HumanPlay
 
 random = RandomStrategy()
 lowest = LowestColumnStrategy()
 human = HumanPlay()
-vertical = VerticalSearcher()
 
-game = Game(lowest,vertical)
-
-game.play(2)
-#game.repeated_play(10)
+game = Game(random,lowest)
+game.play(3)
