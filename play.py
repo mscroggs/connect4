531dc7dd965f52f82a1f2e1c339b274ec4546f1c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2015-06-30 17:50:55 +0100

    fix my stuff

--- play.py
+++ play.py
@@ -3,12 +3,10 @@
 from core import Game
 
 from strategies.random_strategy import RandomStrategy
-from strategies.lowest_column import LowestColumnStrategy
 from strategies.human_play import HumanPlay
 
 random = RandomStrategy()
-lowest = LowestColumnStrategy()
 human = HumanPlay()
 
-game = Game(random,lowest)
-game.play(3)
+game = Game(random,human)
+game.play(1)
