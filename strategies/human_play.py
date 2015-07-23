1b9da714ea0f98700c9b24c29573c21b936b0b64
MScroggs <matthew.w.scroggs@gmail.com>
2015-07-23 17:33:50 +0100

    typo

--- strategies/human_play.py
+++ strategies/human_play.py
@@ -13,7 +13,7 @@ class HumanPlay(Strategy):
         ready = False
         while not ready:
             try:
-                pl = int(raw_input("Please enter the column you would like to play in the press Enter:"))
+                pl = int(raw_input("Please enter the column you would like to play in then press Enter:"))
                 if board[0][pl]==0:
                     ready = True
                 else:
